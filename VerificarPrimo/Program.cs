6c3b36ab5a1ea3c0d48a6dac62d029e0db867763
unknown <jmgonzalez.parana@gmail.com>
2024-05-16 16:55:08 -0300

    primer commit

---
+++ VerificarPrimo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificarPrimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, contD = 0;
+
+            Console.WriteLine("Introducir un número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    contD++;
+                }
+            }
+            if (contD > 2)
+            {
+                Console.WriteLine("El número no es primo");
+                Console.WriteLine("Tiene {0} divisores", contD);
+
+            }
+            else
+            {
+                Console.WriteLine("El número es primo");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
